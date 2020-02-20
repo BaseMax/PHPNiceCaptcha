a5436b0b86f428536adcea59699bbe7f105ea52c
Max Base <Max@Asrez.com>
2020-02-20 13:43:26 +0330

    Create c.php

---
+++ c.php
@@ -0,0 +1,12 @@
+<?php
+function string($input, $strength = 5) {
+	$input_length = strlen($input);
+	$random_string = '';
+	for($i = 0; $i < $strength; $i++) {
+		$random_character = $input[mt_rand(0, $input_length - 1)];
+		$random_string .= $random_character;
+	}
+	return $random_string;
+}
+// ABCDEFGHIJKLMNOPQRSTUVWXYZ
+$captcha_string = string("0123456789", 4);
