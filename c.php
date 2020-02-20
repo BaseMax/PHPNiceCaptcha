190bc7679b75e557866c2dd457fc9147b4ad52cd
Max Base <Max@Asrez.com>
2020-02-20 13:50:16 +0330

    Update c.php

--- c.php
+++ c.php
@@ -15,7 +15,14 @@ $image = imagecreatetruecolor(200, 50);
 imageantialias($image, true);
 //////////////////////////////////////////
 $colors=[
-	imagecolorallocate($image, 50,10,5),
+	#16F292, 22,242,146
+	imagecolorallocate($image, 22,242,146),
+	#b6177b, rgb(182,23,123)
+	imagecolorallocate($image, 182,23,123),
+	#3a2b00, rgb(58,43,0)
+	imagecolorallocate($image, 58,43,0),
+	#9f1435, rgb(159,20,53)
+	imagecolorallocate($image, 159,20,53),
 ];
 imagerectangle($image, 0, 0, rand(40, 190), rand(40, 60), $colors[0]);
 //////////////////////////////////////////
